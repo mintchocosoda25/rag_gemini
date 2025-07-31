06b8f30bf5f3f369df4b2a427fe38e034bb05bce
mintchocosoda25 <165457916+mintchocosoda25@users.noreply.github.com>
2025-07-31 12:55:04 +0900

    Update custom_streamlit_rag_test1.py

--- custom_streamlit_rag_test1.py
+++ custom_streamlit_rag_test1.py
@@ -122,8 +122,8 @@ def get_text(docs):
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
+        chunk_size=200,
+        chunk_overlap=10,
         length_function=tiktoken_len
     )
     chunks = text_splitter.split_documents(text)
