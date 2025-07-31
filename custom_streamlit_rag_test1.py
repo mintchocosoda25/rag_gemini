f68beaf473c6a1df90eaa5cca8874b19086baa80
mintchocosoda25 <165457916+mintchocosoda25@users.noreply.github.com>
2025-07-31 13:03:15 +0900

    Update custom_streamlit_rag_test1.py

--- custom_streamlit_rag_test1.py
+++ custom_streamlit_rag_test1.py
@@ -141,7 +141,7 @@ def get_vectorstore(text_chunks):
     return vectordb
 
 def get_conversation_chain(vetorestore):
-    llm =  ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature = 0)
+    llm =  ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite-preview-06-17", temperature = 0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
